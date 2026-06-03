8ef6d89ca980416bfff77e271e424c97839581ac
Ad7654321 <25024943@woodhouse.ac.uk>
2026-06-03 14:03:13 +0100

    Finished demo with mask and diff using webcam

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import cv2 as cv
+import numpy as np
 
-cap = cv.VideoCapture("SampleClip1.avi")
+kernel = np.ones((3,3), dtype=np.uint8)
+
+cap = cv.VideoCapture(0)
+
+ret, frame_last = cap.read()
+grey_last = cv.cvtColor(frame_last, cv.COLOR_BGR2GRAY)
+
 while cap.isOpened():
+
     ret, frame = cap.read()
+
     if not ret:
         print("No frame received. Exiting... ")
         break
@@ -10,4 +19,20 @@ while cap.isOpened():
     if cv.waitKey(1) == ord("q"):
         break
 
+
+    grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    grey_diff = cv.subtract(grey, grey_last)
+    grey_diff = cv.medianBlur(grey_diff, 3)
+
+    frame_last = frame
+    grey_last = grey
+
+    mask = cv.adaptiveThreshold(grey_diff, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY_INV, 11, 3)
+    mask = cv.medianBlur(mask, 3)
+
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel, iterations=1)
+
     cv.imshow("Frame", frame)
+    cv.imshow("Grey", grey)
+    cv.imshow("grey_diff", grey_diff)
+    cv.imshow("mask", mask)
