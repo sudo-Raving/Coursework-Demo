70c2eba94e286d6231072e9f3122302995755d00
Ad7654321 <25024943@woodhouse.ac.uk>
2026-06-10 18:39:42 +0100

    Started coding classes

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import cv2 as cv
 import numpy as np
 
+class GUI:
+    pass
+
+class MainWindow:
+    pass
+
+class ImageProcessing:
+    def __init__(self):
+        self.cap = cv.VideoCapture(0)
+
+    def displayCap(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            
+            cv.imshow("Frame", frame)
+
+
+img_proc = ImageProcessing()
+img_proc.displayCap()
+    
