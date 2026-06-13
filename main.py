6e1bab717f2e01745ca7eaeba8b2f211264336c6
Ad7654321 <25024943@woodhouse.ac.uk>
2026-06-13 10:35:06 +0100

    Removed GUI and restructured code for the different windows

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import cv2 as cv
 import numpy as np
 import tkinter as tk
-
-class GUI:
-    def __init__(self):
-        pass
-
+    
 class LoginWindow:
     def __init__(self):
         self.window = tk.Tk()
-        passw_entry= tk.Entry(self.window, font = ('calibre',10,'normal'), show = '*')
-        passw_entry.pack()
+        self.passw_entry = tk.Entry(self.window, font = ('calibre',10,'normal'), show = '*')
+        self.passw_entry.pack()
         self.window.geometry("")
         self.window.mainloop()
 
@@ -28,4 +24,4 @@ class ImageProcessing:
             cv.waitKey(1)
             cv.imshow("Frame", frame)
 
-login_win = LoginWindow()
+login = LoginWindow
