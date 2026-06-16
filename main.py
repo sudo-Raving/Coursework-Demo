8f337b9af032358432c17d0ab7bde542b2be24a9
Ad7654321 <25024943@woodhouse.ac.uk>
2026-06-16 15:52:27 +0100

    Created login window and button with input boxes.

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import cv2 as cv
 import numpy as np
 import tkinter as tk
-    
-class LoginWindow:
-    def __init__(self):
+
+class Window:
+    def __init__(self, size="800x600"):
         self.window = tk.Tk()
+        self.window.geometry(size)
+
+
+class LoginWindow(Window):
+    def __init__(self):
+        super().__init__()
+        self.active = True
+        tk.Label(self.window, text="Login", font=("Arial",20)).pack()
+        tk.Label(self.window, text="Username").pack()
+        self.user_entry = tk.Entry(self.window, font=('calibre',10,'normal'))
+        self.user_entry.pack()
+        tk.Label(self.window, text="Password").pack()
         self.passw_entry = tk.Entry(self.window, font = ('calibre',10,'normal'), show = '*')
         self.passw_entry.pack()
-        self.window.geometry("")
+        self.login = tk.Button(self.window, text="Login", font=("Arial", 16), command=self.auth(self.user_entry.get(), self.passw_entry.get()))
+        self.login.pack()
         self.window.mainloop()
 
-class MainWindow:
+    def auth(self, username, password):
+        db = DBConnector()
+
+
+
+class MainWindow(Window):
     def __init__(self):
-        self.window = tk.Tk()
+        super().__init__()
+
+class adminWindow(Window):
+    def __init__(self):
+        super().__init__( )
+
 
 class ImageProcessing:
     def __init__(self):
@@ -24,4 +47,7 @@ class ImageProcessing:
             cv.waitKey(1)
             cv.imshow("Frame", frame)
 
-login = LoginWindow
+class DBConnector:
+    pass
+
+login = LoginWindow()
